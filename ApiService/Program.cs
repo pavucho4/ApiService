42c1d4472b61c7d4f4c2e361ec72089eb5912df8
pavucho4 <101818010+pavucho4@users.noreply.github.com>
2025-05-14 00:51:23 +0300

    Add files via upload

---
+++ ApiService/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using ApiService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers(); // Add support for controllers
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection(); // HTTPS redirection is disabled as per --no-https flag during project creation
+
+app.UseAuthorization();
+
+app.MapControllers(); // Map controller routes
+
+app.Run();
+
